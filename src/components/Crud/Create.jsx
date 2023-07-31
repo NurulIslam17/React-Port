21f4d0818a3f42e00fe416bfa0b086ab117d4771
@Nurul97 <nurulislamnextgenitltd.com>
2023-07-31 12:53:04 +0600

    Crud Store and Delete

--- src/components/Crud/Create.jsx
+++ src/components/Crud/Create.jsx
@@ -1,7 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+// http://localhost:3030/users
 
 function Create() {
+  const [formData, setFormData] = useState({
+    name: "",
+    institute: "",
+    age: "",
+    phone: "",
+    about: "",
+  });
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const postDataToServer = (data) => {
+    const apiUrl = "http://localhost:3030/users";
+    return axios
+      .post(apiUrl, data)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postDataToServer(formData)
+      .then((data) => {
+        console.log("Response from the server:", data);
+        navigate("/crud");
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+      });
+    setMessage("Data Inserted");
+    setFormData([]);
+  };
   return (
     <>
       <section className="my-5">
@@ -13,42 +58,84 @@ function Create() {
                 List
               </Link>
             </div>
+            <p className="text-success">{message ? "Data Inserted" : " "}</p>
           </div>
 
-          <div className="row mt-5">
-            <div className="col-md-5 mx-auto">
+          <div className="row mt-1">
+            <div className="col-md-6 mx-auto">
               <div className="card card-body">
-                <form>
+                <form method="post" onSubmit={handleSubmit}>
                   <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <label htmlFor="exampleInputEmail1">Name</label>
                     <input
-                      type="email"
+                      type="text"
                       className="form-control rounded-0 outline_none mb-2"
                       id="exampleInputEmail1"
                       aria-describedby="emailHelp"
-                      placeholder="Enter email"
+                      placeholder="Enter Name"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Institute</label>
+                    <input
+                      type="text"
+                      className="form-control rounded-0 outline_none mb-2"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter institute"
+                      name="institute"
+                      value={formData.institute}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Age</label>
+                    <input
+                      type="text"
+                      className="form-control rounded-0 outline_none mb-2"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter age"
+                      name="age"
+                      value={formData.age}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Phone</label>
+                    <input
+                      type="text"
+                      className="form-control rounded-0 outline_none mb-2"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter phone"
+                      name="phone"
+                      value={formData.phone}
+                      onChange={handleChange}
                     />
                   </div>
                   <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input
-                      type="password"
+                    <label htmlFor="exampleInputEmail1">About</label>
+                    <textarea
+                      name="about"
+                      value={formData.about}
                       className="form-control rounded-0 outline_none mb-2"
-                      id="exampleInputPassword1"
-                      placeholder="Password"
-                    />
+                      onChange={handleChange}
+                      cols="30"
+                      rows="4"
+                    ></textarea>
                   </div>
-                  <div className="form-group form-check">
-                    <input
-                      type="checkbox"
-                      className="form-check-input rounded-0 outline_none mb-2"
-                      id="exampleCheck1"
-                    />
-                    <label className="form-check-label mb-2" htmlFor="exampleCheck1">
-                      Check me out
-                    </label>
-                  </div>
-                  <button type="submit" className="btn btn-primary rounded-0 outline_none mt-3">
+
+                  <button
+                    type="submit"
+                    className="btn btn-primary rounded-0 outline_none mt-3"
+                  >
                     Save
                   </button>
                 </form>
--- src/components/Crud/List.jsx
+++ src/components/Crud/List.jsx
@@ -1,7 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Loadder from "../Loadder";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 function List() {
+  const [data, setData] = useState([]);
+  const [deleteMsg, setDeleteMsg] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const apiUrl = "http://localhost:3030/users";
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  // Delete Data
+  const deleteDataFromServer = (id) => {
+    const apiUrl = `http://localhost:3030/users/${id}`;
+    return axios
+      .delete(apiUrl)
+      .then((response) => {
+        console.log("Data deleted successfully:", response.data);
+        return response.data;
+      })
+      .catch((error) => {
+        console.error("Error deleting data:", error);
+        throw error;
+      });
+  };
+
+  const handleDelete = (id) => {
+    deleteDataFromServer(id)
+      .then((data) => {
+        console.log("Response from the server:", data);
+        navigate("/crud");
+      })
+      .catch((error) => {
+        console.error("Error occurred:", error);
+      });
+    setDeleteMsg("Success");
+  };
+
+  if (!data) return <Loadder />;
   return (
     <>
       <section className="my-5">
@@ -9,52 +56,53 @@ function List() {
           <div className="row">
             <div className="col-md-12 shadow d-flex justify-content-between py-3">
               <h3>List</h3>
-              <Link to="/create" className="btn btn-sm btn-success pt-2">Add New</Link>
+              <Link to="/create" className="btn btn-sm btn-success pt-2">
+                Add New
+              </Link>
             </div>
+            <p className="text-danger">{deleteMsg ? "Successfull..." : " "}</p>
           </div>
 
           <div className="row mt-5">
             <div className="col-md-12">
               <div className="card card-body">
-                <table className="table text-center table-striped table-bordered py-2" id="myTable">
+                <table
+                  className="table text-center table-striped table-bordered py-2"
+                  id=""
+                >
                   <thead className="bg-info text-center">
                     <tr>
                       <th scope="col text-center">SL</th>
-                      <th scope="col text-center">First</th>
-                      <th scope="col text-center">Last</th>
-                      <th scope="col text-center">Handle</th>
+                      <th scope="col text-center">Name</th>
+                      <th scope="col text-center">Phone</th>
+                      <th scope="col text-center">About</th>
+                      <th scope="col text-center">Age(Year)</th>
                       <th scope="col text-center">Action</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
-                      <td>
-                        <Link className="btn btn-sm btn-success pt-2 me-2">
-                          Edit
-                        </Link>
-                        <Link className="btn btn-sm btn-danger pt-2">
-                          Delete
-                        </Link>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
-                      <td>@fat</td>
-                      <td>
-                        <Link className="btn btn-sm btn-success pt-2 me-2">
-                          Edit
-                        </Link>
-                        <Link className="btn btn-sm btn-danger pt-2">
-                          Delete
-                        </Link>
-                      </td>
-                    </tr>
+                    {data.map((item, index) => {
+                      return (
+                        <tr key={item.id}>
+                          <th scope="row">{index + 1}</th>
+                          <td>{item.name}</td>
+                          <td>{item.phone}</td>
+                          <td>{item.about.slice(0, 50)} .....</td>
+                          <td>{item.age}</td>
+                          <td>
+                            <Link className="btn btn-sm btn-success p-1 me-2">
+                              <FaEdit  size={18} />
+                            </Link>
+                            <Link
+                              onClick={() => handleDelete(item.id)}
+                              className="btn btn-sm btn-danger p-1"
+                            >
+                              <FaTrash size={18}/>
+                            </Link>
+                          </td>
+                        </tr>
+                      );
+                    })}
                   </tbody>
                 </table>
               </div>
